057c1bef0a5683d8b7d949d31b8b156bb9d2f723
Kutaez <m_jasik_2005@mail.ru>
2025-02-14 14:51:30 +0500

    added three beans, set primary bean in configuration.

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -4,15 +4,31 @@ import beans.Smartphone;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.awt.image.ImageProducer;
 
 @Configuration
 @ComponentScan(basePackages = "beans") //added this code because, chtoby ukazat gde nahoditzca component.
     public class AppConfig {
     @Bean
-   Smartphone smartphone(){
-       Smartphone smartphone = new Smartphone();
-       smartphone.setBrand("Samsung");
-       return smartphone;
-   }
-
+    Smartphone samsung() {
+        Smartphone samsung = new Smartphone();
+        samsung.setBrand("Samsung");
+        return samsung;
     }
+
+    @Bean
+    @Primary
+    Smartphone iphone() {
+        Smartphone iphone = new Smartphone();
+        iphone.setBrand("iPhone");
+        return iphone;
+    }
+    Smartphone Xiaomi(){
+        Smartphone xiaomi = new Smartphone();
+        xiaomi.setBrand("Xiaomi");
+        return xiaomi;
+    }
+}
+
