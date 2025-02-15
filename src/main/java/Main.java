c2930b40ae87c133b6519bf18cf339c49d153331
Kutaez <m_jasik_2005@mail.ru>
2025-02-15 08:32:59 +0500

    Redacted all file where write about spring boot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,10 @@ public class Main {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Smartphone smartphone = context.getBean(Smartphone.class);
+        OperatingSystem operatingSystem = context.getBean(OperatingSystem.class);
+        operatingSystem.setType("IOS");
+        System.out.println(operatingSystem.getType());
+        smartphone.setOperatingSystem(operatingSystem);
         System.out.println(smartphone);
-//        OperatingSystem operatingSystem = context.getBean(OperatingSystem.class);
-//        operatingSystem.setType("IOS");
-//        System.out.println(operatingSystem.getType());
-//        smartphone.setOperatingSystem(operatingSystem);
-//        System.out.println(smartphone);
     }
 }
